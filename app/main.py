ea77de1c0c69ce644c58296b94e209d45aa7b370
matheuslmarchetti <lunguinhomarchetti@gmail.com>
2026-02-16 21:36:26 -0400

    Install and config alembic

--- app/main.py
+++ app/main.py
@@ -1,8 +1,5 @@
 from fastapi import FastAPI
 from app.api.v1.router import api_router
-from app.db.session import engine
-from app.db.base import Base
-from app.models import transaction  # importante importar model
 
 app = FastAPI(
     title="Financial Data Pipeline API",
@@ -10,5 +7,3 @@ app = FastAPI(
 )
 
 app.include_router(api_router, prefix="/api/v1")
-
-Base.metadata.create_all(bind=engine)
